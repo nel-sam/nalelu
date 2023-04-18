1c6acba515c42cbb23d9f30130297841fcd0ca9c
Nelson <nelson.rs.07@gmail.com>
2023-04-18 11:34:17 -0700

    Fixed slices

--- lib/state/suuji/count/count_generator.dart
+++ lib/state/suuji/count/count_generator.dart
@@ -144,7 +144,7 @@ class CountGenerator {
           label: NA.t('slices'),
           count: numbofSlices,
           correctAnswers: getNumberOfSlicesAnswerList(numbofSlices),
-          counter: '切れ',
+          counter: '枚',
         );
 
       case 11:
@@ -164,152 +164,6 @@ class CountGenerator {
 
   static int getMaxIndex() {
     return COUNT_EXERCISE_COUNT;
-  }
-
-  static List<String> getNumberOfDaysAnswerList(int numOfDays) {
-    List<String> answerList = [];
-
-    if (numOfDays == 1) {
-      answerList.add('ついたち');
-      answerList.add('一日');
-    } else if (numOfDays == 2) {
-      answerList.add('ふつか');
-      answerList.add('二日');
-    } else if (numOfDays == 3) {
-      answerList.add('みっか');
-      answerList.add('三日');
-    } else if (numOfDays == 4) {
-      answerList.add('よっか');
-      answerList.add('四日');
-    } else if (numOfDays == 5) {
-      answerList.add('いつか');
-      answerList.add('五日');
-    } else if (numOfDays == 6) {
-      answerList.add('むいか');
-      answerList.add('六日');
-    } else if (numOfDays == 7) {
-      answerList.add('なのか');
-      answerList.add('七日');
-    } else if (numOfDays == 8) {
-      answerList.add('ようか');
-      answerList.add('八日');
-    } else if (numOfDays == 9) {
-      answerList.add('ここのか');
-      answerList.add('九日');
-    } else if (numOfDays == 10) {
-      answerList.add('とおか');
-      answerList.add('十日');
-    } else if (numOfDays == 11) {
-      answerList.add('じゅういちにち');
-      answerList.add('十一日');
-    } else if (numOfDays == 12) {
-      answerList.add('じゅうににち');
-      answerList.add('十二日');
-    } else if (numOfDays == 13) {
-      answerList.add('じゅうさんにち');
-      answerList.add('十三日');
-    } else if (numOfDays == 14) {
-      answerList.add('じゅうよんにち');
-      answerList.add('十四日');
-    } else if (numOfDays == 15) {
-      answerList.add('じゅうごにち');
-      answerList.add('十五日');
-    }
-
-    return answerList;
-  }
-
-  static List<String> getNumberOfMonthsAnswerList(int numOfMonths) {
-    List<String> answerList = [];
-
-    if (numOfMonths == 1) {
-      answerList.add('いっかげつ');
-      answerList.add('一ヶ月');
-    } else if (numOfMonths == 2) {
-      answerList.add('にかげつ');
-      answerList.add('二ヶ月');
-    } else if (numOfMonths == 3) {
-      answerList.add('さんかげつ');
-      answerList.add('三ヶ月');
-    } else if (numOfMonths == 4) {
-      answerList.add('よんかげつ');
-      answerList.add('四ヶ月');
-    } else if (numOfMonths == 5) {
-      answerList.add('ごかげつ');
-      answerList.add('五ヶ月');
-    } else if (numOfMonths == 6) {
-      answerList.add('ろっかげつ');
-      answerList.add('六ヶ月');
-    } else if (numOfMonths == 7) {
-      answerList.add('ななかげつ');
-      answerList.add('七ヶ月');
-    } else if (numOfMonths == 8) {
-      answerList.add('はちかげつ');
-      answerList.add('八ヶ月');
-    } else if (numOfMonths == 9) {
-      answerList.add('きゅうかげつ');
-      answerList.add('九ヶ月');
-    } else if (numOfMonths == 10) {
-      answerList.add('じゅうかげつ');
-      answerList.add('十ヶ月');
-    }
-
-    return answerList;
-  }
-
-  static List<String> getNumberOfSmallAnimalsAnswerList(int numOfAnimals) {
-    List<String> answerList = [];
-
-    if (numOfAnimals == 1) {
-      answerList.add('いっぴき');
-      answerList.add('一匹');
-    } else if (numOfAnimals == 2) {
-      answerList.add('にひき');
-      answerList.add('二匹');
-    } else if (numOfAnimals == 3) {
-      answerList.add('さんびき');
-      answerList.add('三匹');
-    } else if (numOfAnimals == 4) {
-      answerList.add('よんひき');
-      answerList.add('四匹');
-    } else if (numOfAnimals == 5) {
-      answerList.add('ごひき');
-      answerList.add('五匹');
-    } else if (numOfAnimals == 6) {
-      answerList.add('ろっぴき');
-      answerList.add('六匹');
-    } else if (numOfAnimals == 7) {
-      answerList.add('ななひき');
-      // answerList.add('しちひき');
-      answerList.add('七匹');
-    } else if (numOfAnimals == 8) {
-      answerList.add('はちひき');
-      // answerList.add('はっぴき');
-      answerList.add('八匹');
-    } else if (numOfAnimals == 9) {
-      answerList.add('きゅうひき');
-      answerList.add('九匹');
-    } else if (numOfAnimals == 10) {
-      answerList.add('じゅっぴき');
-      // answerList.add('じっぴき');
-      answerList.add('十匹');
-    } else if (numOfAnimals == 11) {
-      answerList.add('じゅういっぴき');
-      answerList.add('十一匹');
-    } else if (numOfAnimals == 12) {
-      answerList.add('じゅうにひき');
-      answerList.add('十二匹');
-    } else if (numOfAnimals == 13) {
-      answerList.add('じゅうさんびき');
-      answerList.add('十三匹');
-    } else if (numOfAnimals == 14) {
-      answerList.add('じゅうよんひき');
-      answerList.add('十四匹');
-    } else if (numOfAnimals == 15) {
-      answerList.add('じゅうごひき');
-      answerList.add('十五匹');
-    }
-    return answerList;
   }
 
   static List<String> getNumberOfBigAnimalsAnswerList(int numOfBigAnimals) {
@@ -427,115 +281,6 @@ class CountGenerator {
     return answerList;
   }
 
-  static List<String> getNumberOfPeopleAnswerList(int numOfPeople) {
-    List<String> answerList = [];
-
-    if (numOfPeople == 1) {
-      answerList.add('ひとり');
-      answerList.add('一人');
-    } else if (numOfPeople == 2) {
-      answerList.add('ふたり');
-      answerList.add('二人');
-    } else if (numOfPeople == 3) {
-      answerList.add('さんにん');
-      answerList.add('三人');
-    } else if (numOfPeople == 4) {
-      answerList.add('よにん');
-      answerList.add('四人');
-    } else if (numOfPeople == 5) {
-      answerList.add('ごにん');
-      answerList.add('五人');
-    } else if (numOfPeople == 6) {
-      answerList.add('ろくにん');
-      answerList.add('六人');
-    } else if (numOfPeople == 7) {
-      answerList.add('ななにん');
-      // answerList.add('しちにん');
-      answerList.add('七人');
-    } else if (numOfPeople == 8) {
-      answerList.add('はちにん');
-      answerList.add('八人');
-    } else if (numOfPeople == 9) {
-      answerList.add('くにん');
-      // answerList.add('きゅうにん');
-      answerList.add('九人');
-    } else if (numOfPeople == 10) {
-      answerList.add('じゅうにん');
-      answerList.add('十人');
-    } else if (numOfPeople == 11) {
-      answerList.add('じゅういちにん');
-      answerList.add('十一人');
-    } else if (numOfPeople == 12) {
-      answerList.add('じゅうににん');
-      answerList.add('十二人');
-    } else if (numOfPeople == 13) {
-      answerList.add('じゅうさんにん');
-      answerList.add('十三人');
-    } else if (numOfPeople == 14) {
-      answerList.add('じゅうよにん');
-      answerList.add('十四人');
-    } else if (numOfPeople == 15) {
-      answerList.add('じゅうごにん');
-      answerList.add('十五人');
-    }
-    return answerList;
-  }
-
-  static List<String> getNumberOfThingsAnswerList(int numOfThings) {
-    List<String> answerList = [];
-
-    if (numOfThings == 1) {
-      answerList.add('いっぽん');
-      answerList.add('一本');
-    } else if (numOfThings == 2) {
-      answerList.add('にほん');
-      answerList.add('二本');
-    } else if (numOfThings == 3) {
-      answerList.add('さんぼん');
-      answerList.add('三本');
-    } else if (numOfThings == 4) {
-      answerList.add('よんほん');
-      answerList.add('四本');
-    } else if (numOfThings == 5) {
-      answerList.add('ごにん');
-      answerList.add('五本');
-    } else if (numOfThings == 6) {
-      answerList.add('ろっぽん');
-      answerList.add('六本');
-    } else if (numOfThings == 7) {
-      answerList.add('ななほん');
-      // answerList.add('しちほん');
-      answerList.add('七本');
-    } else if (numOfThings == 8) {
-      answerList.add('はちほん');
-      // answerList.add('はっぽん');
-      answerList.add('八本');
-    } else if (numOfThings == 9) {
-      answerList.add('きゅうほん');
-      answerList.add('九本');
-    } else if (numOfThings == 10) {
-      answerList.add('じゅっぽん');
-      // answerList.add('じっぽん');
-      answerList.add('十本');
-    } else if (numOfThings == 11) {
-      answerList.add('じゅういっぽん');
-      answerList.add('十一本');
-    } else if (numOfThings == 12) {
-      answerList.add('じゅうにほん');
-      answerList.add('十二本');
-    } else if (numOfThings == 13) {
-      answerList.add('じゅうさんほん');
-      answerList.add('十三本');
-    } else if (numOfThings == 14) {
-      answerList.add('じゅうよんほん');
-      answerList.add('十四本');
-    } else if (numOfThings == 15) {
-      answerList.add('じゅうごほん');
-      answerList.add('十五本');
-    }
-    return answerList;
-  }
-
   static List<String> getNumberOfBooksAnswerList(int numOfBooks) {
     List<String> answerList = [];
 
@@ -591,59 +336,56 @@ class CountGenerator {
     return answerList;
   }
 
-  static List<String> getNumberOfSlicesAnswerList(int numOfSlices) {
+  static List<String> getNumberOfDaysAnswerList(int numOfDays) {
     List<String> answerList = [];
 
-    if (numOfSlices == 1) {
-      answerList.add('ひときれ');
-      answerList.add('一切れ');
-    } else if (numOfSlices == 2) {
-      answerList.add('ふたきれ');
-      answerList.add('二切れ');
-    } else if (numOfSlices == 3) {
-      answerList.add('みきれ');
-      answerList.add('三切れ');
-    } else if (numOfSlices == 4) {
-      answerList.add('よんきれ');
-      // answerList.add('よきれ');
-      answerList.add('四切れ');
-    } else if (numOfSlices == 5) {
-      answerList.add('ごきれ');
-      answerList.add('五切れ');
-    } else if (numOfSlices == 6) {
-      answerList.add('ろっきれ');
-      answerList.add('六切れ');
-    } else if (numOfSlices == 7) {
-      answerList.add('ななきれ');
-      answerList.add('七切れ');
-    } else if (numOfSlices == 8) {
-      answerList.add('はちきれ');
-      // answerList.add('はっきれ');
-      answerList.add('八切れ');
-    } else if (numOfSlices == 9) {
-      answerList.add('きゅうきれ');
-      answerList.add('九切れ');
-    } else if (numOfSlices == 10) {
-      answerList.add('じゅっきれ');
-      // answerList.add('じっきれ');
-      answerList.add('十切れ');
-    } else if (numOfSlices == 11) {
-      answerList.add('じゅういっきれ');
-      answerList.add('十一切れ');
-    } else if (numOfSlices == 12) {
-      answerList.add('じゅうにきれ');
-      answerList.add('十二切れ');
-    } else if (numOfSlices == 13) {
-      answerList.add('じゅうさんきれ');
-      answerList.add('十三切れ');
-    } else if (numOfSlices == 14) {
-      answerList.add('じゅうよんきれ');
-      // answerList.add('じゅうよきれ');
-      answerList.add('十四切れ');
-    } else if (numOfSlices == 15) {
-      answerList.add('じゅうごきれ');
-      answerList.add('十五切れ');
+    if (numOfDays == 1) {
+      answerList.add('ついたち');
+      answerList.add('一日');
+    } else if (numOfDays == 2) {
+      answerList.add('ふつか');
+      answerList.add('二日');
+    } else if (numOfDays == 3) {
+      answerList.add('みっか');
+      answerList.add('三日');
+    } else if (numOfDays == 4) {
+      answerList.add('よっか');
+      answerList.add('四日');
+    } else if (numOfDays == 5) {
+      answerList.add('いつか');
+      answerList.add('五日');
+    } else if (numOfDays == 6) {
+      answerList.add('むいか');
+      answerList.add('六日');
+    } else if (numOfDays == 7) {
+      answerList.add('なのか');
+      answerList.add('七日');
+    } else if (numOfDays == 8) {
+      answerList.add('ようか');
+      answerList.add('八日');
+    } else if (numOfDays == 9) {
+      answerList.add('ここのか');
+      answerList.add('九日');
+    } else if (numOfDays == 10) {
+      answerList.add('とおか');
+      answerList.add('十日');
+    } else if (numOfDays == 11) {
+      answerList.add('じゅういちにち');
+      answerList.add('十一日');
+    } else if (numOfDays == 12) {
+      answerList.add('じゅうににち');
+      answerList.add('十二日');
+    } else if (numOfDays == 13) {
+      answerList.add('じゅうさんにち');
+      answerList.add('十三日');
+    } else if (numOfDays == 14) {
+      answerList.add('じゅうよんにち');
+      answerList.add('十四日');
+    } else if (numOfDays == 15) {
+      answerList.add('じゅうごにち');
+      answerList.add('十五日');
     }
+
     return answerList;
   }
 
@@ -697,6 +439,260 @@ class CountGenerator {
     } else if (numOfMachineFurniture == 15) {
       answerList.add('じゅうごだい');
       answerList.add('十五台');
+    }
+    return answerList;
+  }
+
+  static List<String> getNumberOfMonthsAnswerList(int numOfMonths) {
+    List<String> answerList = [];
+
+    if (numOfMonths == 1) {
+      answerList.add('いっかげつ');
+      answerList.add('一ヶ月');
+    } else if (numOfMonths == 2) {
+      answerList.add('にかげつ');
+      answerList.add('二ヶ月');
+    } else if (numOfMonths == 3) {
+      answerList.add('さんかげつ');
+      answerList.add('三ヶ月');
+    } else if (numOfMonths == 4) {
+      answerList.add('よんかげつ');
+      answerList.add('四ヶ月');
+    } else if (numOfMonths == 5) {
+      answerList.add('ごかげつ');
+      answerList.add('五ヶ月');
+    } else if (numOfMonths == 6) {
+      answerList.add('ろっかげつ');
+      answerList.add('六ヶ月');
+    } else if (numOfMonths == 7) {
+      answerList.add('ななかげつ');
+      answerList.add('七ヶ月');
+    } else if (numOfMonths == 8) {
+      answerList.add('はちかげつ');
+      answerList.add('八ヶ月');
+    } else if (numOfMonths == 9) {
+      answerList.add('きゅうかげつ');
+      answerList.add('九ヶ月');
+    } else if (numOfMonths == 10) {
+      answerList.add('じゅうかげつ');
+      answerList.add('十ヶ月');
+    }
+
+    return answerList;
+  }
+
+  static List<String> getNumberOfPeopleAnswerList(int numOfPeople) {
+    List<String> answerList = [];
+
+    if (numOfPeople == 1) {
+      answerList.add('ひとり');
+      answerList.add('一人');
+    } else if (numOfPeople == 2) {
+      answerList.add('ふたり');
+      answerList.add('二人');
+    } else if (numOfPeople == 3) {
+      answerList.add('さんにん');
+      answerList.add('三人');
+    } else if (numOfPeople == 4) {
+      answerList.add('よにん');
+      answerList.add('四人');
+    } else if (numOfPeople == 5) {
+      answerList.add('ごにん');
+      answerList.add('五人');
+    } else if (numOfPeople == 6) {
+      answerList.add('ろくにん');
+      answerList.add('六人');
+    } else if (numOfPeople == 7) {
+      answerList.add('ななにん');
+      // answerList.add('しちにん');
+      answerList.add('七人');
+    } else if (numOfPeople == 8) {
+      answerList.add('はちにん');
+      answerList.add('八人');
+    } else if (numOfPeople == 9) {
+      answerList.add('くにん');
+      // answerList.add('きゅうにん');
+      answerList.add('九人');
+    } else if (numOfPeople == 10) {
+      answerList.add('じゅうにん');
+      answerList.add('十人');
+    } else if (numOfPeople == 11) {
+      answerList.add('じゅういちにん');
+      answerList.add('十一人');
+    } else if (numOfPeople == 12) {
+      answerList.add('じゅうににん');
+      answerList.add('十二人');
+    } else if (numOfPeople == 13) {
+      answerList.add('じゅうさんにん');
+      answerList.add('十三人');
+    } else if (numOfPeople == 14) {
+      answerList.add('じゅうよにん');
+      answerList.add('十四人');
+    } else if (numOfPeople == 15) {
+      answerList.add('じゅうごにん');
+      answerList.add('十五人');
+    }
+    return answerList;
+  }
+
+  static List<String> getNumberOfSlicesAnswerList(int numOfSlices) {
+    List<String> answerList = [];
+
+    if (numOfSlices == 1) {
+      answerList.add('いちまい');
+      answerList.add('一枚');
+    } else if (numOfSlices == 2) {
+      answerList.add('にまい');
+      answerList.add('二一枚');
+    } else if (numOfSlices == 3) {
+      answerList.add('さんまい');
+      answerList.add('三枚');
+    } else if (numOfSlices == 4) {
+      answerList.add('よんまい');
+      answerList.add('四枚');
+    } else if (numOfSlices == 5) {
+      answerList.add('ごまい');
+      answerList.add('五枚');
+    } else if (numOfSlices == 6) {
+      answerList.add('ろくまい');
+      answerList.add('六枚');
+    } else if (numOfSlices == 7) {
+      answerList.add('ななまい');
+      answerList.add('七枚');
+    } else if (numOfSlices == 8) {
+      answerList.add('はちまい');
+      answerList.add('八枚');
+    } else if (numOfSlices == 9) {
+      answerList.add('きゅうまい');
+      answerList.add('九枚');
+    } else if (numOfSlices == 10) {
+      answerList.add('じゅうまい');
+      answerList.add('十枚');
+    } else if (numOfSlices == 11) {
+      answerList.add('じゅういちまい');
+      answerList.add('十一枚');
+    } else if (numOfSlices == 12) {
+      answerList.add('じゅうにまい');
+      answerList.add('十二枚');
+    } else if (numOfSlices == 13) {
+      answerList.add('じゅうさんまい');
+      answerList.add('十三枚');
+    } else if (numOfSlices == 14) {
+      answerList.add('じゅうよんまい');
+      answerList.add('十四枚');
+    } else if (numOfSlices == 15) {
+      answerList.add('じゅうごまい');
+      answerList.add('十五枚');
+    }
+    return answerList;
+  }
+
+  static List<String> getNumberOfSmallAnimalsAnswerList(int numOfAnimals) {
+    List<String> answerList = [];
+
+    if (numOfAnimals == 1) {
+      answerList.add('いっぴき');
+      answerList.add('一匹');
+    } else if (numOfAnimals == 2) {
+      answerList.add('にひき');
+      answerList.add('二匹');
+    } else if (numOfAnimals == 3) {
+      answerList.add('さんびき');
+      answerList.add('三匹');
+    } else if (numOfAnimals == 4) {
+      answerList.add('よんひき');
+      answerList.add('四匹');
+    } else if (numOfAnimals == 5) {
+      answerList.add('ごひき');
+      answerList.add('五匹');
+    } else if (numOfAnimals == 6) {
+      answerList.add('ろっぴき');
+      answerList.add('六匹');
+    } else if (numOfAnimals == 7) {
+      answerList.add('ななひき');
+      // answerList.add('しちひき');
+      answerList.add('七匹');
+    } else if (numOfAnimals == 8) {
+      answerList.add('はちひき');
+      // answerList.add('はっぴき');
+      answerList.add('八匹');
+    } else if (numOfAnimals == 9) {
+      answerList.add('きゅうひき');
+      answerList.add('九匹');
+    } else if (numOfAnimals == 10) {
+      answerList.add('じゅっぴき');
+      // answerList.add('じっぴき');
+      answerList.add('十匹');
+    } else if (numOfAnimals == 11) {
+      answerList.add('じゅういっぴき');
+      answerList.add('十一匹');
+    } else if (numOfAnimals == 12) {
+      answerList.add('じゅうにひき');
+      answerList.add('十二匹');
+    } else if (numOfAnimals == 13) {
+      answerList.add('じゅうさんびき');
+      answerList.add('十三匹');
+    } else if (numOfAnimals == 14) {
+      answerList.add('じゅうよんひき');
+      answerList.add('十四匹');
+    } else if (numOfAnimals == 15) {
+      answerList.add('じゅうごひき');
+      answerList.add('十五匹');
+    }
+    return answerList;
+  }
+
+  static List<String> getNumberOfThingsAnswerList(int numOfThings) {
+    List<String> answerList = [];
+
+    if (numOfThings == 1) {
+      answerList.add('いっぽん');
+      answerList.add('一本');
+    } else if (numOfThings == 2) {
+      answerList.add('にほん');
+      answerList.add('二本');
+    } else if (numOfThings == 3) {
+      answerList.add('さんぼん');
+      answerList.add('三本');
+    } else if (numOfThings == 4) {
+      answerList.add('よんほん');
+      answerList.add('四本');
+    } else if (numOfThings == 5) {
+      answerList.add('ごにん');
+      answerList.add('五本');
+    } else if (numOfThings == 6) {
+      answerList.add('ろっぽん');
+      answerList.add('六本');
+    } else if (numOfThings == 7) {
+      answerList.add('ななほん');
+      // answerList.add('しちほん');
+      answerList.add('七本');
+    } else if (numOfThings == 8) {
+      answerList.add('はちほん');
+      // answerList.add('はっぽん');
+      answerList.add('八本');
+    } else if (numOfThings == 9) {
+      answerList.add('きゅうほん');
+      answerList.add('九本');
+    } else if (numOfThings == 10) {
+      answerList.add('じゅっぽん');
+      // answerList.add('じっぽん');
+      answerList.add('十本');
+    } else if (numOfThings == 11) {
+      answerList.add('じゅういっぽん');
+      answerList.add('十一本');
+    } else if (numOfThings == 12) {
+      answerList.add('じゅうにほん');
+      answerList.add('十二本');
+    } else if (numOfThings == 13) {
+      answerList.add('じゅうさんほん');
+      answerList.add('十三本');
+    } else if (numOfThings == 14) {
+      answerList.add('じゅうよんほん');
+      answerList.add('十四本');
+    } else if (numOfThings == 15) {
+      answerList.add('じゅうごほん');
+      answerList.add('十五本');
     }
     return answerList;
   }
