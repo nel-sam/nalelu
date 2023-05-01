2da0b71b2ab294c44506a230c557971829833a7c
Byron <47398200+byronxc@users.noreply.github.com>
2023-05-01 14:53:55 -0700

    added adCard to the main menu

--- lib/widgets/menus/main_menu.dart
+++ lib/widgets/menus/main_menu.dart
@@ -5,6 +5,7 @@ import 'package:nalelu/na_helpers.dart';
 import 'package:nalelu/widgets/doushi_exercise/doushi_exercise_1.dart';
 import 'package:nalelu/widgets/manga_exercise/manga_exercise.dart';
 import 'package:nalelu/widgets/menus/numbers_menu.dart';
+import 'package:nalelu/widgets/shared/ad_card.dart';
 import 'package:nalelu/widgets/shared/na_menu_button.dart';
 import 'package:nrs_flutter_lib/constants.dart';
 import 'package:nrs_flutter_lib/nrs_flutter_lib.dart';
@@ -61,6 +62,7 @@ class MainMenu extends StatelessWidget {
             ],
           ),
         ),
+        const AdCard(),
       ],
     );
   }
