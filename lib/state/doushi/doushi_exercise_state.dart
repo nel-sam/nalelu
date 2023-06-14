526b696b86a716d6d61c8c216dcbd0d7d6137746
Byron <47398200+byronxc@users.noreply.github.com>
2023-06-14 15:46:14 -0700

    fixes

--- lib/state/doushi/doushi_exercise_state.dart
+++ lib/state/doushi/doushi_exercise_state.dart
@@ -1,33 +1,22 @@
+import 'dart:collection';
+
 import 'package:nalelu/lang_data/doushi.dart';
 
 class DoushiExerciseState {
-  List<String> userInput = [];
+  HashMap<int, String> userInput = new HashMap<int, String>();
   Doushi doushi;
 
   DoushiExerciseState({required this.doushi});
 
   getUserInput(int index) {
-    fillArray(index);
-    return userInput[index];
-  }
-
-  void fillArray(int index) {
-    if (userInput.length == 0) {
-      userInput.add('');
-    }
-
-    while (userInput.length <= index) {
-      userInput.add('');
-    }
+    return userInput[index] ?? '';
   }
 
   updateUserInput(int index, String newValue) {
-    fillArray(index);
     userInput[index] = newValue;
   }
 
   clear() {
     userInput.clear();
   }
-
 }
