eaa158876147ccc01ce92733ac2147a12da9ae2a
Byron <47398200+byronxc@users.noreply.github.com>
2023-06-16 17:38:42 -0700

    Remodeled doushi and manga

--- lib/widgets/doushi_exercise/doushi_exercise_1.dart
+++ lib/widgets/doushi_exercise/doushi_exercise_1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nalelu/lang_data/doushi.dart';
 import 'package:nalelu/na_helpers.dart';
 import 'package:nalelu/state/doushi/doushi_notifier.dart';
 import 'package:nalelu/state/enums.dart';
@@ -12,87 +13,87 @@ import 'package:nrs_flutter_lib/widgets/n_footer_button.dart';
 import 'package:nrs_flutter_lib/widgets/n_footer_menu.dart';
 import 'package:provider/provider.dart';
 
-class DoushiExerciseLevel1 extends StatefulWidget {
+class DoushiExerciseLevel1 extends StatelessWidget {
   final bool showVerbFurigana;
   final bool showVerbTranslations;
   final int numberOfDoushiExercises;
 
-  const DoushiExerciseLevel1(
-      {Key? key,
-      required this.showVerbFurigana,
+  DoushiExerciseLevel1(
+      {required this.showVerbFurigana,
       required this.showVerbTranslations,
-      required this.numberOfDoushiExercises})
-      : super(key: key);
+      required this.numberOfDoushiExercises});
 
-  @override
-  State<DoushiExerciseLevel1> createState() => _DoushiExerciseLevel1State();
-}
-
-class _DoushiExerciseLevel1State extends State<DoushiExerciseLevel1> {
   @override
   Widget build(BuildContext context) {
+    ExerciseNavNotifier nav =
+        ExerciseNavNotifier(exerciseType: ExerciseType.Doushi);
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      child: ChangeNotifierProvider<ExerciseNavNotifier>(
-        create: (context) => ExerciseNavNotifier(
-            exerciseType: ExerciseType.Doushi,
-            maxExerciseCount: widget.numberOfDoushiExercises),
-        child: Consumer<ExerciseNavNotifier>(
-          builder: (context, navNotifier, child) => Scaffold(
-            appBar: Nrs.NrsAppBar(title: NA.t('verbs'), context: context),
-            body: Center(
-              child: CustomScrollView(
-                slivers: [
-                  SliverFillRemaining(
-                    hasScrollBody: false,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        NavHeaderWrapper(navNotifier: navNotifier),
-                        Expanded(
-                            child: Container(
-                          child: ChangeNotifierProvider<DoushiNotifier>(
-                            create: (context) =>
-                                DoushiNotifier(navNotifier.getActive),
-                            child: Consumer<DoushiNotifier>(
-                              builder: (context, doushiNotifier, child) =>
-                                  DoushiExerciseStateArea(
-                                      state: doushiNotifier.getActive(),
-                                      showVerbTranslations:
-                                          widget.showVerbTranslations,
-                                      showVerbFurigana:
-                                          widget.showVerbFurigana),
-                            ),
-                          ),
-                        )),
-                        AdCard(),
-                      ],
+      child: Scaffold(
+        appBar: Nrs.NrsAppBar(title: NA.t('verbs'), context: context),
+        body: Center(
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ChangeNotifierProvider<ExerciseNavNotifier>(
+                      create: (context) => ExerciseNavNotifier(
+                          exerciseType: ExerciseType.Doushi,
+                          maxExerciseCount: numberOfDoushiExercises),
+                      child: Consumer<ExerciseNavNotifier>(
+                        builder: (context, navNotifier, child) {
+                          nav = navNotifier;
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              NavHeaderWrapper(navNotifier: navNotifier),
+                              ChangeNotifierProvider<DoushiNotifier>(
+                                create: (context) =>
+                                    DoushiNotifier(navNotifier.getActive),
+                                child: Consumer<DoushiNotifier>(
+                                  builder: (context, doushiNotifier, child) {
+                                    return DoushiExerciseStateArea(
+                                        state: doushiNotifier.getActive(),
+                                        showVerbTranslations:
+                                            showVerbTranslations,
+                                        showVerbFurigana: showVerbFurigana);
+                                  },
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
                     ),
-                  )
-                ],
-              ),
-            ),
-            persistentFooterButtons: [
-              NFooterMenu(
-                buttons: [
-                  NFooterButton(
-                    text: NA.t('verbChart'),
-                    icon: Icons.list,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              VerbChart(doushi: navNotifier.getActive().doushi),
-                        ),
-                      );
-                    },
-                  ),
-                ],
+                    AdCard(),
+                  ],
+                ),
               ),
             ],
           ),
         ),
+        persistentFooterButtons: [
+          NFooterMenu(
+            buttons: [
+              NFooterButton(
+                text: NA.t('verbChart'),
+                icon: Icons.list,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          VerbChart(doushi: nav.getActive().doushi),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
